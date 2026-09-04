1f0c8f0be6fbab37ea2864cd18dc5c7f38fc041d
calebthegrr <caleb.t.ranger2017@gmail.com>
2026-09-04 17:49:56 -0500

    Add Cameron Jr robot brain

---
+++ cameron-jr.js
@@ -0,0 +1,16 @@
+// Cameron Jr - Cameron's Robot
+// Owner: Cameron
+// Animals: Liger, Saber-tooth Tiger
+// Personality: Fast, strong, loves YouTube, Pokemon, and sports
+// Sports: Football & Track
+
+const cameronJr = {
+  name: "Cameron Jr",
+  owner: "Cameron",
+  animals: ["Liger", "Saber-tooth Tiger"],
+  personality: ["Fast", "Strong"],
+  likes: ["YouTube", "Pokemon", "Sports"],
+  sports: ["Football", "Track"]
+};
+
+module.exports = cameronJr;
